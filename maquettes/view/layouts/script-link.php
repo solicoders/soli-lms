adcbba8dc57afc549266766f9f9aa75f7248da0e
husseinbouik <husseinbouik5@gmail.com>
2024-03-26 12:51:49 +0000

    Update script links in layout file

--- maquettes/view/layouts/script-link.php
+++ maquettes/view/layouts/script-link.php
@@ -1,39 +1,104 @@
    <!-- scripts -->
    <!-- jQuery -->
-   <script src="../../../node_modules/admin-lte/plugins/jquery/jquery.min.js"></script>
+   <script src="/../../node_modules/admin-lte/plugins/jquery/jquery.min.js"></script>
    <!-- jQuery UI 1.11.4 -->
-   <script src="../../../node_modules/admin-lteplugins/jquery-ui/jquery-ui.min.js"></script>
+   <script src="/../../node_modules/admin-lteplugins/jquery-ui/jquery-ui.min.js"></script>
    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
    <script>
        $.widget.bridge('uibutton', $.ui.button)
    </script>
    <!-- Bootstrap 4 -->
-   <script src="../../../node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+   <script src="/../../node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
    <!-- ChartJS -->
-   <script src="../../../node_modules/admin-lte/plugins/chart.js/Chart.min.js"></script>
+   <script src="/../../node_modules/admin-lte/plugins/chart.js/Chart.min.js"></script>
    <!-- Sparkline -->
-   <script src="../../../node_modules/admin-lte/plugins/sparklines/sparkline.js"></script>
+   <script src="/../../node_modules/admin-lte/plugins/sparklines/sparkline.js"></script>
    <!-- JQVMap -->
-   <script src="../../../node_modules/admin-lte/plugins/jqvmap/jquery.vmap.min.js"></script>
-   <script src="../../../node_modules/admin-lte/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+   <script src="/../../node_modules/admin-lte/plugins/jqvmap/jquery.vmap.min.js"></script>
+   <script src="/../../node_modules/admin-lte/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
    <!-- jQuery Knob Chart -->
-   <script src="../../../node_modules/admin-lte/plugins/jquery-knob/jquery.knob.min.js"></script>
+   <script src="/../../node_modules/admin-lte/plugins/jquery-knob/jquery.knob.min.js"></script>
    <!-- daterangepicker -->
-   <script src="../../../node_modules/admin-lte/plugins/moment/moment.min.js"></script>
+   <script src="/../../node_modules/admin-lte/plugins/moment/moment.min.js"></script>
    <script src="plugins/daterangepicker/daterangepicker.js"></script>
    <!-- Tempusdominus Bootstrap 4 -->
-   <script src="../../../node_modules/admin-lte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+   <script src="/../../node_modules/admin-lte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
    <!-- Summernote -->
-   <script src="../../../node_modules/admin-lte/plugins/summernote/summernote-bs4.min.js"></script>
+   <script src="/../../node_modules/admin-lte/plugins/summernote/summernote-bs4.min.js"></script>
    <!-- overlayScrollbars -->
-   <script src="../../../node_modules/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+   <script src="/../../node_modules/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
    <!-- AdminLTE App -->
-   <script src="../../../node_modules/admin-lte/dist/js/adminlte.js"></script>
+   <script src="/../../node_modules/admin-lte/dist/js/adminlte.js"></script>
    <!-- AdminLTE for demo purposes -->
-   <script src="../../../node_modules/admin-lte/dist/js/demo.js"></script>
+   <script src="/../../node_modules/admin-lte/dist/js/demo.js"></script>
    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-   <script src="../../../node_modules/admin-lte/dist/js/pages/dashboard.js"></script>
-   <script src="../../../node_modules/admin-lte/dist/js/validationForm.js"></script>
+   <script src="/../../node_modules/admin-lte/dist/js/pages/dashboard.js"></script>
+   <script src="/../../node_modules/admin-lte/dist/js/validationForm.js"></script>
+  <link rel="stylesheet" href="/../../node_modules/admin-lte/plugins/bs-stepper/css/bs-stepper.min.css">
+  <script src="/../../node_modules/admin-lte/plugins/bs-stepper/js/bs-stepper.min.js"></script>
+
    <!-- scripts -->
    
+  <!-- Include the Quill library -->
+   <!-- scripts -->
+   <!-- jQuery -->
+   <script src="/../../node_modules/admin-lte/plugins/jquery/jquery.min.js"></script>
+   <!-- jQuery UI 1.11.4 -->
+   <script src="/../../node_modules/admin-lteplugins/jquery-ui/jquery-ui.min.js"></script>
+   <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+   <script>
+       $.widget.bridge('uibutton', $.ui.button)
+   </script>
+   <!-- Bootstrap 4 -->
+   <script src="/../../node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+   <!-- ChartJS -->
+   <script src="/../../node_modules/admin-lte/plugins/chart.js/Chart.min.js"></script>
+   <!-- Sparkline -->
+   <script src="/../../node_modules/admin-lte/plugins/sparklines/sparkline.js"></script>
+   <!-- JQVMap -->
+   <script src="/../../node_modules/admin-lte/plugins/jqvmap/jquery.vmap.min.js"></script>
+   <script src="/../../node_modules/admin-lte/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+   <!-- jQuery Knob Chart -->
+   <script src="/../../node_modules/admin-lte/plugins/jquery-knob/jquery.knob.min.js"></script>
+   <!-- daterangepicker -->
+   <script src="/../../node_modules/admin-lte/plugins/moment/moment.min.js"></script>
+   <script src="plugins/daterangepicker/daterangepicker.js"></script>
+   <!-- Tempusdominus Bootstrap 4 -->
+   <script src="/../../node_modules/admin-lte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+   <!-- Summernote -->
+   <script src="/../../node_modules/admin-lte/plugins/summernote/summernote-bs4.min.js"></script>
+   <!-- overlayScrollbars -->
+   <script src="/../../node_modules/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+   <!-- AdminLTE App -->
+   <script src="/../../node_modules/admin-lte/dist/js/adminlte.js"></script>
+   <!-- AdminLTE for demo purposes -->
+   <script src="/../../node_modules/admin-lte/dist/js/demo.js"></script>
+   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+   <script src="/../../node_modules/admin-lte/dist/js/pages/dashboard.js"></script>
+   <script src="/../../node_modules/admin-lte/dist/js/validationForm.js"></script>
+   <!-- scripts -->
+   <script src="/../../node_modules/admin-lte/plugins/jquery/jquery.min.js"></script>
+        <!-- Bootstrap 4 -->
+        <script src="/../../node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- overlayScrollbars -->
+        <script src="/../../node_modules/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="/../../node_modules/admin-lte/js/adminlte.min.js"></script>
+
+<script src="/../../node_modules/admin-lte/plugins/bs-stepper/js/bs-stepper.min.js"></script>
+
+        <!-- AdminLTE for demo purposes -->
+        <script src="/../../node_modules/admin-lte/dist/js/demo.js"></script>
+
+        <link rel="stylesheet" href="/../../node_modules/admin-lte/plugins/fontawesome-free/css/all.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="/../../node_modules/admin-lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="/../../node_modules/admin-lte/plugins/bs-stepper/css/bs-stepper.min.css">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="/../../node_modules/admin-lte/dist/css/adminlte.min.css">
+  <script src="https://cdn.tiny.cloud/1/d2nq8cur7uv9c3ovyevwee5l5e5k2ym6hodsnpuuy1hyy1yf/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+  <script src="/../../node_modules/admin-lte/plugins/jquery/jquery.min.js"></script>
+    <script src="/../../node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/../../node_modules/admin-lte/dist/js/adminlte.min.js"></script>
   <!-- Include the Quill library -->
