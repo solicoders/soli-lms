47766aad5474f47d51633269d14f006754fb6976
husseinbouik <husseinbouik5@gmail.com>
2024-04-05 09:55:22 +0000

    Refactor session handling and update user menu

--- maquettes/view/layouts/nav.php
+++ maquettes/view/layouts/nav.php
@@ -1,12 +1,5 @@
-<?php
-session_start();
-
-if (isset($_POST['logout'])) {
-    session_destroy();
-    $_SESSION = array();
-    header('Location: ../../../../index.php');
-    exit();
-}
+<?php 
+    session_start();
 ?>
 <!-- <nav class="main-header navbar navbar-expand navbar-white navbar-light"> -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -21,39 +14,24 @@ if (isset($_POST['logout'])) {
 
         <li class="nav-item dropdown user-menu">
             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                <img src="/view/assets/images/man.png" class="user-image img-circle elevation-2"
-                    alt="Image d'utilisateur">
-                <span class="d-none d-md-inline">
-                    <?php
-                    if (isset($_SESSION['email']) && $_SESSION['email'] === 'apprenant@solicode.com') {
-                        echo 'hussein bouik';
-                    } else {
-                        echo 'Formateur';
-                    }
-                    ?>
-                </span>
+                <img src="/view/assets/images/man.png" class="user-image img-circle elevation-2" alt="Image d'utilisateur">
+                <span class="d-none d-md-inline">Formateur</span>
             </a>
             <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                 <!-- Image d'utilisateur -->
                 <li class="user-header bg-info">
                     <img src="/view/assets/images/man.png" class="img-circle elevation-2" alt="Image d'utilisateur">
                     <p>
-                        <?php
-                        if (isset($_SESSION['email']) && $_SESSION['email'] === 'apprenant@solicode.com') {
-                            echo 'hussein bouik';
-                        } else {
-                            echo 'Formateur';
-                        }
-                        ?> <small>Membre depuis le 28/12/2023</small>
+                    Formateur
+                        <small>Membre depuis le 28/12/2023</small>
                     </p>
                 </li>
                 <!-- Pied de page du menu -->
                 <li class="user-footer">
-                    <form id="logout-form" action="" method="POST">
-                        <a href="#" class="btn btn-default btn-flat">Profile</a>
-                        <input type="submit" name="logout" value="Déconnexion"
-                            class="btn btn-default btn-flat float-right logout">
-                    </form>
+                    <a href="#" class="btn btn-default btn-flat">Profil</a>
+                    <a href="/index.php" class="btn btn-default btn-flat float-right">
+                        Se déconnecter
+                    </a>
                 </li>
             </ul>
 
