b05597b7333ba47b8b9dbcf29f5891865bb2d2e3
Amina Assaid <aminaassaid123@gmail.com>
2024-04-19 16:50:02 +0100

    Save

--- app/database/migrations/GestionCompetences/2024_04_19_141324_modules.php
+++ app/database/migrations/GestionCompetences/2024_04_19_141324_modules.php
@@ -15,38 +15,8 @@ return new class extends Migration
             $table->string('Nom');
             $table->string('Description');
             $table->string('masse_horaire');
-            use Illuminate\Database\Migrations\Migration;
-            use Illuminate\Database\Schema\Blueprint;
-            use Illuminate\Support\Facades\Schema;
-
-            return new class extends Migration
-            {
-                /**
-                 * Run the migrations.
-                 */
-                public function up(): void
-                {
-                    Schema::create('Modules', function (Blueprint $table) {
-                        $table->id();
-                        $table->string('Nom');
-                        $table->string('Description');
-                        $table->string('masse_horaire');
-                        $table->unsignedBigInteger('Competences_id');
-                        $table->foreign('Competences_id')->references('id')->on('Competences')->onDelete('cascade');
-                        $table->timestamps();
-                    });
-
-                }
-
-                /**
-                 * Reverse the migrations.
-                 */
-                public function down(): void
-                {
-                    Schema::dropIfExists('Modules');
-                }
-            };
-
+            $table->unsignedBigInteger('Competences_id');
+            $table->foreign('Competences_id')->references('id')->on('Competences')->onDelete('cascade');
             $table->timestamps();
         });
 
