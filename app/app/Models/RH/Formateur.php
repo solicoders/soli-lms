bcd2b53c1563375da81d64ecc92ac6488b19d6e2
husseinbouik <husseinbouik5@gmail.com>
2024-04-19 16:22:17 +0100

    Add RH models: Apprenant and Formateur

---
+++ app/app/Models/RH/Formateur.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\RH;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+
+class Formateur extends User
+{
+    use HasFactory;
+}
