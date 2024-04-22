109a04f8d10d227fd9d92e988ad24e6b21dc3e76
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2024-04-22 19:18:14 +0100

    Refactor user table migration to rename 'nom' and 'prenom' columns to 'name'

--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nom');
-            $table->string('prenom');
-            $table->string('type')->nullable();
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
