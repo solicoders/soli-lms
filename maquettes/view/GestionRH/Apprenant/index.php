2615dde6361bb8bbeb12f98237f77780012886d9
Reda Grain <grain.reda.solicode@gmail.com>
2024-04-22 13:57:17 +0100

    add folders and index

---
+++ maquettes/view/GestionRH/Apprenant/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once "../../layouts/heade.php" ?>
+
+<body class="sidebar-mini" style="height: auto;">
+
+<!-- Site wrapper -->
+<div class="wrapper">
+
+    <!-- Navigation -->
+    <?php include_once "../../layouts/nav.php" ?>
+    <!-- Barre latérale -->
+    <?php include_once "../../layouts/aside.php" ?>
+
+    <div class="content-wrapper">
+
+        <section class="content-header">
+            <div class="container-fluid ">
+                <div class="row mb-2">
+                    <div class="col-sm-3">
+                        <h1>Liste des Apprenants</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+            </div>
+        </section>
+        <!-- /.content -->
+    </div>
+  
+</div>
+</body>
+
+<?php include_once "../../layouts/script-link.php"; ?>
+</html>
