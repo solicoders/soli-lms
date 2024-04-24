d6e56faea60a3d6be663473eb56829e59862380f
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2024-04-24 22:46:40 +0100

    Refactor seeders and models namespaces in GestionCompetences directory

--- app/database/seeders/GestionCompetences/CompetencesSeeder.php
+++ app/database/seeders/GestionCompetences/CompetencesSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\GestionCompetences;
+namespace Database\Seeders\GestionCompetences;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
