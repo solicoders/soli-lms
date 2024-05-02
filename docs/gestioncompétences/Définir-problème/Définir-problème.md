50a33de3cd054f264b4891e1ae665eb892e62664
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2024-05-02 12:15:58 +0100

    Remove unnecessary files and directories

--- docs/gestioncompétences/Définir-problème/Définir-problème.md
+++
@@ -1,3 +0,0 @@
-# Définir problème Gestion compétences
-
-  - Chaque formateur et responsable de formation devrait avoir une connaissance approfondie de chaque filière, des modules, des compétences et des niveaux correspondants, avec leurs définitions et leur masse horaire
--- maquettes/view/autorisation/Controllers/form.php
+++
@@ -1,7 +0,0 @@
-<form>
-    <div class="form-group">
-        <label for="rActionName">Nom de Controller</label>
-        <input type="text" class="form-control" id="rActionName" placeholder="Entrez le nom de l'action" value="ProjetsController">
-    </div>
-    <button type="submit" class="btn btn-info">Ajouter</button>
-</form>
