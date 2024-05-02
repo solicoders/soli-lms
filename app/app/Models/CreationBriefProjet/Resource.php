c6ccc692f1e21a650343b4a093a5468777f0ec50
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2024-05-02 16:56:33 +0100

    Update Resource model to remove unnecessary fillable field

--- app/app/Models/CreationBriefProjet/Resource.php
+++ app/app/Models/CreationBriefProjet/Resource.php
@@ -8,15 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Resource extends Model
 {
     use HasFactory;
-    protected $fillable = ['nom', 'description', 'lien','nature', 'brief_projet_id'];
+    protected $fillable = ['nom', 'description', 'lien', 'brief_projet_id'];
 
     public function briefProjet()
     {
         return $this->belongsTo(BriefProjet::class);
     }
 }
-php artisan make:exception CreationBriefProjet/BriefProjetAlreadyExistException
-php artisan make:exception CreationBriefProjet/AffectationCompetenceAlreadyExistException
-php artisan make:exception CreationBriefProjet/EtatRealisationAlreadyExistException
-php artisan make:exception CreationBriefProjet/LivrableAlreadyExistException
-php artisan make:exception CreationBriefProjet/ResourceAlreadyExistException
