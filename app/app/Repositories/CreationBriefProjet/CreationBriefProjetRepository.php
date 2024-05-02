e2ef365c3aa32ed5355c5494b26291fe9d97c0d7
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2024-05-02 12:49:39 +0100

    Add CreationBriefProjetRepository class for handling creation of brief projects

---
+++ app/app/Repositories/CreationBriefProjet/CreationBriefProjetRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories\CreationBriefProjet;
+
+use App\Models\CreationBriefProjet\BriefProjet;
+use App\Repositories\BaseRepositorie;
+use Illuminate\Database\Eloquent\Model;
+use App\Exceptions\CreationBriefProjet\BriefProjetAlreadyExistException;
+
+class CreationBriefProjet extends BaseRepositorie
+{
+    protected $model;
+
+    public function __construct(BriefProjet $briefprojet)
+    {
+        $this->model = $briefprojet;
+    }
+    public function create(array $data)
+    {
+        $titre = $data['titre'];
+
+        $existingbriefprojet = BriefProjet::where('titre', $titre)->exists();
+
+        if ($existingbriefprojet) {
+            throw BriefProjetAlreadyExistException::createProject();
+        } else {
+            return parent::create($data);
+        }
+    }
+    public function searchData($searchableData, $perPage = 4)
+    {
+        return $this->model->where(function ($query) use ($searchableData) {
+            $query->where('titre', 'like', '%' . $searchableData . '%')
+                ->orWhere('description', 'like', '%' . $searchableData . '%');
+        })->paginate($perPage);
+    }
+}
