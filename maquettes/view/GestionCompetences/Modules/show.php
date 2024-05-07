b63ee06c72abf8dc745a4f35f7cd182dd260425f
Amina Assaid <aminaassaid123@gmail.com>
2024-05-07 16:54:02 +0100

    save

--- maquettes/view/GestionCompetences/Modules/show.php
+++ maquettes/view/GestionCompetences/Modules/show.php
@@ -10,58 +10,55 @@
         <!-- Barre latérale -->
         <?php include_once "../../layouts/aside.php" ?>
 
-        
-
 
         <div class="content-wrapper">
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>Détail de module</h1>
-                </div>
-                
-            </div>
-        </div>
-    </div>
-<section class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-body">
-                            <div class="col-sm-12">
-                                <label for="nom">Filieres</label>
-                                <p>Développeur Mobile – mode Bootcamp</p>
-                            </div>
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Détail de module</h1>
+                        </div>
 
-                            
-                            <div class="col-sm-12">
-                                <label for="description">N°</label>
-                                    <p>M01</p>
-                            </div>
-
-                            
-                            <div class="col-sm-12">
-                                <label for="description">MODULE</label>
-                                <p>Communication écrite et orale</p>
-                            </div>
-
-                            <div class="col-sm-12">
-                                <label for="description">Description</label>
-                                <p>Ceci est la description du module.</p>
-                            </div>
-                        
                     </div>
                 </div>
             </div>
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="col-sm-12">
+                                        <label for="nom">Filieres</label>
+                                        <p>Développeur Mobile – mode Bootcamp</p>
+                                    </div>
+
+
+                                    <div class="col-sm-12">
+                                        <label for="description">N°</label>
+                                        <p>M01</p>
+                                    </div>
+
+                                    <div class="col-sm-12">
+                                        <label for="description">MODULE</label>
+                                        <p>Communication écrite et orale</p>
+                                    </div>
+
+                                    <div class="col-sm-12">
+                                        <label for="description">Description</label>
+                                        <p>Ceci est la description du module.</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
-    </div>
-</section>
-</div>
 
 
-        
+
     </div>
 
     </div>
