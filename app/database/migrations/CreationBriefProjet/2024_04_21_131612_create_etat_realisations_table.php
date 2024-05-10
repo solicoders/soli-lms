4868c73f0c52994b3eaa62835af4160005b880e9
husseinbouik <husseinbouik5@gmail.com>
2024-05-10 15:55:35 +0100

    save

--- app/database/migrations/CreationBriefProjet/2024_04_21_131612_create_etat_realisations_table.php
+++ app/database/migrations/CreationBriefProjet/2024_04_21_131612_create_etat_realisations_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        
         Schema::create('etat_realisations', function (Blueprint $table) {
             $table->id();
             $table->string('nom');
