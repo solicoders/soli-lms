16838bab37277c680a5d3b9133633e03b82a985a
husseinbouik <husseinbouik5@gmail.com>
2024-05-10 16:24:56 +0100

    chore: Add navigation menu for managing briefs and realizations

--- app/resources/views/layouts/menu.blade.php
+++ app/resources/views/layouts/menu.blade.php
@@ -17,4 +17,30 @@
         </p>
     </a>
 </li>
+          <li class="nav-item has-treeview">
+            <a href="#"
+              class="nav-link ">
+              <i class="nav-icon fas fa-project-diagram"></i>
+              <p>
+                Gestion des briefs
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="/view/GestionBriefProjet/Formateur/GestionBriefs/index.php"
+                  class="nav-link  ">
+                  <i class="far fa-file-alt nav-icon"></i>
+                  <p>Briefs</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="/view/GestionBriefProjet/Formateur/Realisations/index.php"
+                  class="nav-link  ">
+                  <i class="far fa-chart-bar nav-icon"></i>
+                  <p>Réalisations</p>
+                </a>
+              </li>
+            </ul>
+          </li>
 {{-- @endcan --}}
