23db8c304566c46cf1fb7035c0c18f8924345a24
Reda Grain <grain.reda.solicode@gmail.com>
2024-05-13 16:51:50 +0200

    undo edit users

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -7,19 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use App\Models\GestionRH\Apprenant;
-use App\Models\GestionRH\Formateur;
-use App\Models\GestionRH\Ville;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
-    // protected $table = 'users';
 
     public const ADMIN = "admin";
     public const MEMBRE = "membre";
-    public const RESPONSABLE_FORMATION = "responsable_formation";
 
     /**
      * The attributes that are mass assignable.
@@ -27,17 +22,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'nom',
-        'prenom',
-        'nom_arab',
-        'prenom_arab',
-        'cin',
-        'date_naissance',
-        'tele_num',
-        'rue',
-        'ville_id',
-        'profile_image',
-        'type',
+        'name',
         'email',
         'password',
     ];
@@ -60,22 +45,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-
-
-
-    public function Apprenant()
-    {
-        return $this->hasMany(Apprenant::class);
-    }
-
-    public function Formateur()
-    {
-        return $this->hasMany(Formateur::class);
-    }
-
-    public function Ville()
-    {
-        return $this->hasMany(Ville::class);
-    }
 }
