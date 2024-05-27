4b423d836c72a06a6ae9be2eae4ba629f12e1982
husseinbouik <husseinbouik5@gmail.com>
2024-05-27 10:46:45 +0100

    feat: Add seeders for RH package

---
+++ app/database/seeders/pkg_creation_projets/TransfertCompetenceSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders\pkg_creation_projets;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TransfertCompetenceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
---
+++ app/database/seeders/pkg_rh/SpecialiteSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders\pkg_rh;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SpecialiteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
