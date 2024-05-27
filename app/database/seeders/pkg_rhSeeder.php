7e72550503da5bf7397f8444411b8505b3c6ab6a
Reda Grain <grain.reda.solicode@gmail.com>
2024-05-28 00:53:57 +0200

    update

--- app/database/seeders/pkg_rhSeeder.php
+++ app/database/seeders/pkg_rhSeeder.php
@@ -2,6 +2,13 @@
 
 namespace Database\Seeders;
 
+use Database\Seeders\pkg_rh\VilleSeeder;
+use Database\Seeders\pkg_rh\SpecialiteSeeder;
+use Database\Seeders\pkg_rh\NiveauScholaireSeeder;
+use Database\Seeders\pkg_rh\GroupeSeeder;
+use Database\Seeders\pkg_rh\PersonneSeeder;
+use Database\Seeders\pkg_rh\FormateurSeeder;
+use Database\Seeders\pkg_rh\ApprenantSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +17,16 @@ class pkg_rhSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        return [
+            VilleSeeder::class,
+            SpecialiteSeeder::class,
+            NiveauScholaireSeeder::class,
+            GroupeSeeder::class,
+            PersonneSeeder::class,
+            FormateurSeeder::class,
+            ApprenantSeeder::class,
+        ];
     }
 }
