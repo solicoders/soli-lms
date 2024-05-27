43a5e03d3157a45599785138273e404cbe2e0605
husseinbouik <husseinbouik5@gmail.com>
2024-05-27 10:42:57 +0100

    feat: Add new models for every package

---
+++ app/app/Models/pkg_competences/Technologie.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\pkg_competences;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Technologie extends Model
+{
+    use HasFactory;
+}
---
+++ app/app/Models/pkg_formations/Formation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\pkg_formations;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Formation extends Model
+{
+    use HasFactory;
+}
