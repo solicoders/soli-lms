c8699758a080521b6fe857ef16fa1b8fc2e95c5a
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-05-27 13:57:01 +0100

    add

--- app/app/Models/pkg_validations/Validation.php
+++ app/app/Models/pkg_validations/Validation.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\pkg_validations;
 use App\Models\pkg_creation_projets\TransfertCompetence ;
+use App\Models\pkg_realisation_projets\RealisationProjet;
 use App\Models\pkg_competences\Appreciation;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -10,11 +11,11 @@ class Validation extends Model
 {
     use HasFactory;
 
-
     protected $fillable = [
         'note',
         'transfert_competence_id',
         'appreciation_id',
+        'realisation_projet_id',
     ];
 
     public function transfertCompetence()
@@ -25,6 +26,11 @@ class Validation extends Model
     public function appreciation()
     {
         return $this->belongsTo(Appreciation::class);
+    }
+
+    public function realisationProjet()
+    {
+        return $this->belongsTo(RealisationProjet::class);
     }
 
     public function messages()
