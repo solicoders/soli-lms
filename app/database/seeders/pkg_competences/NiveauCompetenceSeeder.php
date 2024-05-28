220661fcb6cc17b16b5c90f6fd1ce958afc582d9
Amina Assaid <aminaassaid123@gmail.com>
2024-05-28 06:43:46 +0100

    CategorieTechnologieSeeder

--- app/database/seeders/pkg_competences/NiveauCompetenceSeeder.php
+++ app/database/seeders/pkg_competences/NiveauCompetenceSeeder.php
@@ -18,6 +18,7 @@ class NiveauCompetencesSeeder extends Seeder
      */
     public function run(): void
     {
+
         Schema::disableForeignKeyConstraints();
         NiveauCompetence::truncate();
         Schema::enableForeignKeyConstraints();
