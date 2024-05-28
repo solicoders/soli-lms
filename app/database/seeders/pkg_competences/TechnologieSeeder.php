65a1a547ee89475cbe8d94a53c454b4918790768
Amina Assaid <aminaassaid123@gmail.com>
2024-05-28 06:51:50 +0100

    TechnologieSeeder

--- app/database/seeders/pkg_competences/TechnologieSeeder.php
+++ app/database/seeders/pkg_competences/TechnologieSeeder.php
@@ -2,16 +2,43 @@
 
 namespace Database\Seeders\pkg_competences;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Permission;
+use App\Models\pkg_competences\Technologie;
+use App\Models\pkg_notifications\Notification;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Spatie\Permission\Models\Role;
 
-class TechnologieSeeder extends Seeder
+class TechnologiesSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+
+        // ==========================================================
+        // ================= Add Seeder Technologies ================
+        // ==========================================================
+        Schema::disableForeignKeyConstraints();
+        Technologie::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $csvFile = fopen(base_path("database/data/pkg_competences/technologies/Technologies.csv"), "r");
+        $firstline = true;
+        while (($data = fgetcsv($csvFile)) !== FALSE) {
+            if (!$firstline) {
+                Technologie::create([
+                    "nom" => $data['0'],
+                    "description" => $data['1'],
+                    "categorie_technologies_id" => $data['2'],
+                ]);
+            }
+            $firstline = false;
+        }
+        fclose($csvFile);
     }
 }
