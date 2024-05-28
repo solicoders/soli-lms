b640fde7f0b7f2781fbf1d04cade58400d8091f6
Safaâ Faiz <88689147+Safaa1faiz@users.noreply.github.com>
2024-05-28 12:01:36 +0100

    save

--- app/database/migrations/pkg_formations/2024_05_27_091034_create_formations_table.php
+++ app/database/migrations/pkg_formations/2024_05_27_091034_create_formations_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('formations', function (Blueprint $table) {
             $table->id();
+            $table->string('nom');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
