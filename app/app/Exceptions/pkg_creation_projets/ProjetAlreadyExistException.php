91f9e2ac8949518a1a12e5bf09164c37198adddc
husseinbouik <husseinbouik5@gmail.com>
2024-05-30 11:44:41 +0100

    Add new repository files for pkg_creation_projets

--- app/app/Exceptions/pkg_creation_projets/ProjetAlreadyExistException.php
+++ app/app/Exceptions/pkg_creation_projets/ProjetAlreadyExistException.php
@@ -4,7 +4,9 @@ namespace App\Exceptions\pkg_creation_projets;
 
 use Exception;
 
-class ProjetAlreadyExistException extends Exception
+class ProjetAlreadyExistException extends BusinessException
 {
-    //
-}
+    public static function createProject()
+    {
+        return new self(__('pkg_creation_projets/message.createProjectException'));
+    }}
