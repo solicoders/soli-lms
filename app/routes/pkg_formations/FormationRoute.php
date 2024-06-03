962769cd74b9ac00efd370dc598212d7819122cd
safaa1faiz <Safaa1faiz@users.noreply.github.com>
2024-06-04 00:24:31 +0200

    save

--- app/routes/pkg_formations/FormationRoute.php
+++ app/routes/pkg_formations/FormationRoute.php
@@ -7,4 +7,7 @@ Route::prefix('formations')->group(function () {
     Route::post('/', [FormationController::class, 'store'])->name('formations.store');
     Route::put('/{id}', [FormationController::class, 'update'])->name('formations.update');
     Route::delete('/{id}', [FormationController::class, 'destroy'])->name('formations.destroy');
+    Route::get('formations/export', [FormationController::class, 'export'])->name('formations.export');
+    Route::post('formations/import', [FormationController::class, 'import'])->name('formations.import');
+
 });
