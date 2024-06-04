c0a5b557fefacdf04b5c4411586575d238f8fcd6
husseinbouik <husseinbouik5@gmail.com>
2024-06-04 16:46:30 +0100

    Refactor app.js to import additional scripts for dashboard functionality

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -4,17 +4,14 @@ import 'jquery/dist/jquery';
 // Import AdminLTE
 import "admin-lte/dist/js/adminlte";
 import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle';
-import 'admin-lte/dist/js/adminlte';
 import 'admin-lte/dist/js/demo.js';
 import 'admin-lte/dist/js/pages/dashboard.js';
-
 
 
 // Importation de CKEditor
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 document.addEventListener("DOMContentLoaded", function () {
-    window.stepper = new Stepper(document.querySelector('.bs-stepper'))
 
     // Initialisation de CKEditor
     ClassicEditor.create(document.querySelector("#editor"))
