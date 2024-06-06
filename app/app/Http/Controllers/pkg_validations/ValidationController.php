a428c79e9bd2ca8980945369c02f12a6f980cf02
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-06-06 13:27:14 +0100

    save

--- app/app/Http/Controllers/pkg_validations/ValidationController.php
+++ app/app/Http/Controllers/pkg_validations/ValidationController.php
@@ -8,7 +8,14 @@ use App\Repositories\pkg_validations\ValidationsRepository;
 use App\Models\pkg_validations\Validation;
 use App\Models\pkg_realisation_projets\RealisationProjet;
 use App\Models\pkg_realisation_projets\LivrableRealisation;
+use App\Models\pkg_creation_projets\TransfertCompetence;
+use App\Models\pkg_creation_projets\Projet;
 use App\Models\pkg_competences\Competence;
+use App\Models\pkg_competences\Appreciation;
+use App\Models\pkg_validations\Message;
+// use Illuminate\Http\Request;
+
+
 
 
 class ValidationController extends Controller
@@ -31,18 +38,24 @@ class ValidationController extends Controller
     public function show($realisation_projet_id)
     {
         $realisation = RealisationProjet::find($realisation_projet_id);
-        // dd($realisation);
-        $Competences = Competence::all() ; // Assuming a relationship exists
-        // dd($Competences);
-
-        
+        $TransfertCompetence = TransfertCompetence::find($realisation_projet_id);
+        $Competences = Competence::find($TransfertCompetence); 
         $LivrableRealisation = LivrableRealisation::find($realisation_projet_id);
-        // dd($LivrableRealisation->lien);
-    
-        return view('pkg_validations.index', compact('realisation' ,'Competences'  ,'LivrableRealisation' )
+        $appreciations = Appreciation::all();
+        $validations = Validation::find($realisation_projet_id);
+         $Projet = Projet::all();
+         $messages = Message::find($realisation_projet_id);
+        return view('pkg_validations.index', compact('realisation' ,'Competences'  ,'LivrableRealisation','appreciations','validations','Projet','messages')
 
         );
     }
+
+
+    public function store(Request $request){
+        dd($request);
+        
+    }
+    
 
     public function create()
     {
